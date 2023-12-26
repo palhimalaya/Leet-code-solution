19a2acbf0f10e370a10a748effd971665bdb7de7
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2023-12-26 04:52:02 +0000

    Add project files and settings

---
+++ C#/DisplayTheUniqueElements/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] arr = new int[10] {3, 5, 2, 7, 7, 4, 1, 5, 7, 2};
+        bool[] visitedNumber = new bool[arr.Length];
+        bool isUniq;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!visitedNumber[i])
+            {
+                isUniq = true;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        isUniq = false;
+                        visitedNumber[j] = true;
+                    }
+                }
+
+                if (isUniq)
+                {
+                    Console.WriteLine("{0} ", arr[i]);
+                }
+            }
+        }
